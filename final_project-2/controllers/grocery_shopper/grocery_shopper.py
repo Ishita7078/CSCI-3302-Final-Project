53bd8d3fc52c4a25d8ec05b4bd183a4cebcd83b6
IshitaM <78755481+Ishita7078@users.noreply.github.com>
2025-04-14 15:36:38 -0600

    Add files via upload

--- final_project-2/controllers/grocery_shopper/grocery_shopper.py
+++ final_project-2/controllers/grocery_shopper/grocery_shopper.py
@@ -5,6 +5,7 @@
 from controller import Robot
 import math
 import numpy as np
+from iterative_closest_point import icp_matching
 
 #Initialization
 print("=== Initializing Grocery Shopper...")
@@ -18,12 +19,17 @@ N_PARTS = 12
 LIDAR_ANGLE_BINS = 667
 LIDAR_SENSOR_MAX_RANGE = 5.5 # Meters
 LIDAR_ANGLE_RANGE = math.radians(240)
+print(LIDAR_ANGLE_BINS)
+lidar_offsets = np.linspace(-LIDAR_ANGLE_RANGE / 2., LIDAR_ANGLE_RANGE / 2., LIDAR_ANGLE_BINS)
+lidar_offsets = lidar_offsets[::-1]
 
 # create the Robot instance.
 robot = Robot()
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
+
+
 
 # The Tiago robot has multiple motors, each identified by their names below
 part_names = ("head_2_joint", "head_1_joint", "torso_lift_joint", "arm_1_joint",
@@ -52,7 +58,7 @@ right_gripper_enc.enable(timestep)
 # Enable Camera
 camera = robot.getDevice('camera')
 camera.enable(timestep)
-camera.recognitionEnable(timestep)
+# camera.recognitionEnable(timestep)
 
 # Enable GPS and compass localization
 gps = robot.getDevice("gps")
@@ -68,6 +74,9 @@ lidar.enablePointCloud()
 # Enable display
 display = robot.getDevice("display")
 
+keyboard = robot.getKeyboard()
+keyboard.enable(timestep)
+
 # Odometry
 pose_x     = 0
 pose_y     = 0
@@ -76,22 +85,63 @@ pose_theta = 0
 vL = 0
 vR = 0
 
-lidar_sensor_readings = [] # List to hold sensor readings
 lidar_offsets = np.linspace(-LIDAR_ANGLE_RANGE/2., +LIDAR_ANGLE_RANGE/2., LIDAR_ANGLE_BINS)
-lidar_offsets = lidar_offsets[83:len(lidar_offsets)-83] # Only keep lidar readings not blocked by robot chassis
+lidar_offsets = lidar_offsets[83:len(lidar_offsets)-83] #provides clearest image
 
-map = None
+def get_pose(gps, compass): #webots provided pose, CHANGE
+    x_r = gps.getValues()[0]
+    y_r = gps.getValues()[1]
+    theta_r = np.arctan2(compass.getValues()[0], compass.getValues()[1])
+    return x_r, y_r, theta_r
 
-
+def rotate(ranges, angles): #rotate angles appropriately
+    xs = ranges * np.cos(angles)
+    ys = ranges * np.sin(angles)
+    return np.vstack((xs, ys))
 
 # ------------------------------------------------------------------
 # Helper Functions
 
+poses = []  #list of (x, y, theta)
+scans = []  #list of LIDAR
+last_scan = None
+last_pose = None
 
 gripper_status="closed"
 
 # Main Loop
 while robot.step(timestep) != -1:
+        
+    pose_x, pose_y, pose_theta = get_pose(gps, compass) #webots pose, CHANGE
+    lidar_values = np.array(lidar.getRangeImage())
+    
+    scan = lidar_values[83:-83]  #clearest image
+
+    valid = np.isfinite(scan) #filter invalid readings
+    filtered_ranges = scan[valid]
+    filtered_angles = lidar_offsets[valid]    
+    
+    if len(filtered_ranges) > 10:
+        curr = rotate(filtered_ranges, filtered_angles)
+    
+        if 'pc_last' in locals(): #if previous scan, apply icp
+            try:
+                R, t = icp_matching(pc_last, curr)
+                dtheta = math.atan2(R[1, 0], R[0, 0])
+                dx, dy = t[0, 0], t[1, 0]
+    
+                pose_theta += dtheta
+                pose_x += dx * math.cos(pose_theta) - dy * math.sin(pose_theta)
+                pose_y += dx * math.sin(pose_theta) + dy * math.cos(pose_theta)
+    
+            except Exception as e: #to catch improperly formatted dimensions or non-convergence of SVD
+                print("[ICP Error]", e)
+    
+        pc_last = curr
+    
+    #update last scan and pose
+    last_scan = scan
+    last_pose = (pose_x, pose_y, pose_theta)
     
     
     robot_parts["wheel_left_joint"].setVelocity(vL)
